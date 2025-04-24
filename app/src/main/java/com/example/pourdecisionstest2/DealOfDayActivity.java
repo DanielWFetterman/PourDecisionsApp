50c97008a4c64ef99f6e2efa41237f6a3c1016d9
Dylan <61261822+Jalpster@users.noreply.github.com>
2025-04-24 16:13:05 -0400

    new awesome screen

---
+++ app/src/main/java/com/example/pourdecisionstest2/DealOfDayActivity.java
@@ -0,0 +1,16 @@
+package com.example.pourdecisionstest2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DealOfDayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.deal_of_the_day);
+    }
+}
