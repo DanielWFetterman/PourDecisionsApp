b7b6412e20aadb9109c6b8e3d43de916e05b7fad
Dylan <61261822+Jalpster@users.noreply.github.com>
2025-04-29 16:13:52 -0400

    menu to all screens

--- app/src/main/java/com/example/pourdecisionstest2/DealOfDayActivity.java
+++ app/src/main/java/com/example/pourdecisionstest2/DealOfDayActivity.java
@@ -1,8 +1,10 @@
 package com.example.pourdecisionstest2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,5 +14,20 @@ public class DealOfDayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.deal_of_the_day);
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // Close this activity and return to previous
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/pourdecisionstest2/DealsActivity.java
+++ app/src/main/java/com/example/pourdecisionstest2/DealsActivity.java
@@ -2,7 +2,10 @@
 package com.example.pourdecisionstest2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -23,5 +26,20 @@ public class DealsActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recycler_deals);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new DealsAdapter(deals));
+
+        Toolbar toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // Close this activity and return to previous
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
